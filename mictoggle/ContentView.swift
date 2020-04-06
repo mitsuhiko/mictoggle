02c04ff11a7b53809bc4b4e4d634d2592aaf894d
Armin Ronacher <armin.ronacher@active-4.com>
2020-04-06 14:15:59 +0200

    Initial Commit

---
+++ mictoggle/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  mictoggle
+//
+//  Created by Armin Ronacher on 06.04.20.
+//  Copyright © 2020 Armin Ronacher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
